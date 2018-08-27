9d337ca303d0b9e53080c8a961ba34a6886453cb
Diogo Vanderlan <diogo.vanderlan@gmail.com>
2018-08-27 20:50:30 -0300

    Trabalho do galvao concluido

--- docs/exemplo.php
+++ docs/exemplo.php
@@ -3,7 +3,7 @@ require '../autoload.php';
 use Turma3\Config;
 use Turma3\Base;
 
-$config = new Config('mysql', 'localhost', 3306, 'turma3', 'root', 'minhasenha');
+$config = new Config('mysql', 'localhost', 3306, 'vanderlan', 'root', '');
 $dbh = new Base($config);
 
 $stmt = $dbh->preparar(sprintf("INSERT INTO categoria (nome) VALUES ('%s')", $_POST['nome']));
